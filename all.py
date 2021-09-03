7a3bef436b84557a4d62db999deb387b60e89b5c
diego axsel garcia <diego.garciasi@autonoma.edu.co>
2021-09-03 10:48:07 -0500

    Actualización del codigo ahora se puede conectar a un dase de datos remota a travez de ssh tunel

--- all.py
+++ all.py
@@ -6,8 +6,7 @@ algoritmo unifcador de todo lo que se hizo
 
 @author: Matador
 """
-#%% librerias
-import mysql.connector as sql # conectar a base de datos
+#%% librerias de python
 import pandas as pd # manejo de dataframes
 from sklearn.preprocessing import OrdinalEncoder# hacer encoder sobre las variables string
 from sklearn.model_selection import train_test_split # para partir las variables
@@ -16,275 +15,13 @@ import json # importar archivos json
 from progress.bar import Bar #animacion de barra de carga
 import numpy as np # manejo de herramientas matematicas y control de arrays
 from datetime import datetime
+
+# librerias propias
+from Lib.sql_operations import admin
+import Lib.conect as conect
 #%% objetos
 #%%
 #objeto para importar la informacion desde la base de datos
-class admin():    
-    #incializar variables
-    def __init__(self,user,passwd,name_database,host):   
-        """[inicializacion de objeto que se conecta con la base de datos]
-
-        Args:
-            user ([string]): [usuario registrado en la base de datos]
-            passwd ([string]): [contraseña relacionada con el usuario]
-            name_database ([string]): [nombre de la base de datos]
-            host ([string]): [puerto al que se conecta]
-        """
-             
-        self.user = user
-        self.passwd = passwd
-        self.name_database = name_database
-        self.host = host
-        
-    #conectar con la base de datos
-    
-    def conect(self): # conectarse con la base de datos   
-        """[conectarse con la base de datos]
-
-        Returns:
-            [database]: [devuelva la base de datos]
-        """
-         
-        self.db = sql.connect(
-                user = self.user,
-                passwd = self.passwd,
-                database = self.name_database,
-                host = self.host
-                )
-        return self.db
-    
-   # realiza la consulta 
-    def query(self,query):
-        """[crea el cursor y ejecuta la consulta enviada]
-
-        Returns:
-            [list of tuples]: [la informacion obtenida de la consulta]
-        """
-        
-        self.c = self.db.cursor()
-        self.c.execute(query)
-        
-        return self.c.fetchall()
-    
-    def create_table(self,table_name,variables_names,variables_types):
-        """[crea una tabla en la base de datos]
-
-        Args:
-            table_name ([string]): [nombre de la tabla a crear]
-            variables_names ([list]): [nombre de las variables de la tabla]
-            variables_types ([list]): [nombre de los tipos de variables]
-
-        Returns:
-            [boolean]: [un estado si la tabla ya existia]
-        """
-        
-        
-        if(self.table_exist(table_name)  == True):
-            
-            my_query = "CREATE TABLE "
-            my_query += table_name
-            my_query += " (id{} INT AUTO_INCREMENT PRIMARY KEY,".format(table_name)
-                        
-            for i,j in zip(variables_names, variables_types):
-                
-                name = " " + i +" "
-                Type = j  +" NOT NULL "+ ","
-                
-                my_query = my_query + name + Type
-            
-            my_query = my_query[:len(my_query) - 1]
-            
-            my_query += ")"
-            
-            self.c.execute(my_query)
-            
-            print("table has been created")
-            return True
-            
-        else:
-            
-            print ("table already exist")
-            return False
-                
-    def table_exist(self,name):
-        """[revisa si la tabla existe en la base de datos]
-
-        Args:
-            name ([string]): [nombre de la tabla a consultar]
-
-        Returns:
-            [boolean]: [estado de la consulta]
-        """
-        
-        self.c.execute("SHOW TABLES")
-        
-        tables_names = [i[0] for i in self.c]
-        
-        if name not in tables_names:
-            return True
-        else:
-            return False
-        
-    def add_foreigh_key(self,tablep,tables):
-        """[agrega una llave foranea con respecto a las id de las tablas
-
-        Args:
-            tablep ([string]): [nombre de la tabla principal]
-            tables ([type]): [nombre  de la tabla secundaria]
-        """
-        
-        query = "ALTER TABLE " + tables
-         
-        ID0 = "(" + tablep + "_id" + tablep + ")" 
-        ID1 = "(id" + tablep+ ")"
-         
-        query += " ADD FOREIGN KEY " + ID0 + " REFERENCES " + tablep + ID1 
-         
-        self.c.execute(query)
-         
-    def drop_table(self,table_name):
-        """[elimina una tabla]
-
-        Args:
-            table_name ([string]): [nombre de la tabla a eliminar]
-        """
-        
-        query = "DROP TABLE IF EXISTS {}".format(table_name)
-        self.c.execute(query)
-        
-    def drop_column(self,table_name,column_name):
-        """[elimina una columna de una tabla]
-
-        Args:
-            table_name ([string]): [nombre de la tabla]
-            column_name ([string]): [nombre de la column]
-        """
-        query = "ALTER TABLE {} DROP COLUMN {}".format(table_name, column_name)
-        
-        self.c.execute(query)
-        
-        print("column has been deleted")
-        
-    def add_column(self,table_name,column_name,types):
-        """[agrega columna a una tabla]
-
-        Args:
-            table_name ([string]): [nombre de la tabla]
-            column_name ([string]): [nombre de la nueva columna]
-            types ([string]): [nombre del tipo de dato de la columna]
-        """
-        
-        query = "ALTER TABLE {} ADD {} {}".format(table_name,column_name,types)
-        self.c.execute(query)
-        
-    def is_empty(self,table_name):
-        """[revisa si una tabla se encuentra vacia]
-
-        Args:
-            table_name ([string]): [nombre de la columna]
-
-        Returns:
-            [int]: [numero de registro dentro de la tabla]
-        """
-        
-        query = "SELECT * FROM {}".format(table_name)
-        
-        self.c.execute(query)
-        self.c.fetchall()
-        
-        return self.c.rowcount
-    
-    
-    def add_info(self,table_name,columns_name,vals,value):
-        """[agrega informacion dentro de la tabla]
-
-        Args:
-            table_name ([string]): [nombre de la tabla]
-            columns_name ([list]]): [nombre de la columna a modificar o insertar]
-            vals ([list]): [valores a ingresar]
-            value ([boolean]): [revisar si la tabla apenas se creo o ya lo habia hecho]
-        """
-    
-        if value ==   True:
-            
-            query = "INSERT INTO {} ".format(table_name)
-            
-            f = "("
-            s = "("
-            for i in columns_name:
-                f += i + ", "
-                s += "%s, "
-                
-            f = f[:len(f) - 2]
-            f += ")"
-            
-            s = s[:len(s) - 2]
-            s += ")"
-            
-            query += f + " VALUES " + s
-            
-            val = (vals[0],vals[1],int(vals[2]))
-            self.c.execute(query,val)
-            
-            self.db.commit()
-            
-        else:
-            
-            check_info = [columns_name[2],vals[2]]
-            
-            for col,val in zip(columns_name,vals):
-                
-                update_info = [col,val]
-                self.update(table_name,update_info,check_info)
-            
-    def update(self,table_name, update_info, check_info):#funcion que actualiza los valores
-        """[actualiza los datos de la tabla]
-
-        Args:
-            table_name ([string]): [nombre de la tabla]
-            update_info ([list]): [informacion para actualizar]
-            check_info ([list]): [informacion de condicion donde actualizar los datos]
-        """
-        if(isinstance(update_info[1], str)):
-        
-            query = "UPDATE {} SET {} = '{}' WHERE {} = {}".format(table_name,update_info[0],update_info[1],check_info[0],check_info[1])
-            
-            self.c = self.db.cursor(buffered = True)
-            self.c.execute(query)
-            self.db.commit()
-
-#organizador con respecto a lo enviado
-class organizer():
-    
-    def __init__(self,result,columns):
-        """[Organizar la base de datos]
-
-        Args:
-            result ([dataframe]): [description]
-            columns ([list]): [description]
-        """
-        self.messy = result
-        self.columns = columns
-    
-    def sort(self):
-        """[ordenar los datos]
-
-        Returns:
-            [dataframe]: [datos ordenados]
-        """
-        #crea un dicticcionario
-        sort = {}
-        
-        #llenamos de listas
-        for i in self.columns:
-            sort[i] = []
-        
-        #ordenamos para luego generar un dataframe
-        for i in self.messy:
-            for jj,j in enumerate(self.columns):
-                sort[j].append(i[jj])
-        
-        return pd.DataFrame(sort)
 
 class subdivider():#subdivide los datos ordenados en categorias para trabajar individualmente
     
@@ -370,6 +107,7 @@ class Encoder():
     
     def encoder_y(self):
         return self.enc_y
+    
 #objeto que transforma las varables de fechas a dias           
 class DateTime():
 
@@ -474,13 +212,19 @@ def change_date(db, New_date):
 
 class my_main():
     
-    def __init__(self):
+    def __init__(self,cursor,connection):
         
-        f1 = open("Json/input.json")
-        self.data1 = json.load(f1)
+        # f1 = open("Json/input.json")
+        # self.data1 = json.load(f1)
         
         f2 = open("Json/predict.json")
         self.data2 = json.load(f2)
+        
+        #cursor que realizara las operaciones diferentes a las consultas con salida de tabla
+        self.c = cursor
+        
+        #variable para hacer las consultas de tablas
+        self.db = connection
         
         # consulta que se realiza
         self.my_query = """SELECT 
@@ -518,16 +262,19 @@ class my_main():
         modelo.marca_idmarca = marca.idmarca"""
                 
         self.columns = ["id",
-                "modelo",
-                "area",
-                "marca",
-                "nombre_sede",
-                "categoria",
-                "fecha_registro",
-                "fecha_mant",
-                "observaciones"]
+                        "modelo",
+                        "area",
+                        "marca",
+                        "nombre_sede",
+                        "categoria",
+                        "fecha_registro",
+                        "fecha_mant",
+                        "observaciones"]
         
-        self.names = ["modelo","area","nombre_sede","marca"]
+        self.names = ["modelo",
+                      "area",
+                      "nombre_sede",
+                      "marca"]
                 
         self.train_model()
         
@@ -537,15 +284,18 @@ class my_main():
             self.predict_few()
 
     def train_model(self):
+        """[entrena el modelo]
+        """
         #lectura de los datos que se encuentran en json
         self.obj_mod = {}
         
         #conectando con la base de datos
-        self.Admin = admin(self.data1["user"],self.data1["passwd"] ,self.data1["database"],self.data1["host"])
-        self.Admin.conect()
+        self.Admin = admin(self.db,
+                           self.c)
 
         #realiza la consulta
-        result = self.Admin.query(self.my_query)
+        result = conect.run_query(self.my_query,
+                                  self.db)
         
         #tamaño minino de muestras
         tam = 40
@@ -568,7 +318,8 @@ class my_main():
         bar.finish()
              
     def predict_and_go(self):
-        
+        """[predice y envia los datos a una tabla]
+        """
         # los encoders ya se encuentran construidos
         predict_data = {}
         col_names = self.enc_X_data.keys()
@@ -578,8 +329,8 @@ class my_main():
         
         for i in col_names:
             
-            self.enc_X_data[i] = self.enc_X_data[i].drop_duplicates(subset = ["id"])
-            self.sub_data[i] = self.sub_data[i].drop_duplicates(subset = ["id"])
+            self.enc_X_data[i] = self.enc_X_data[i].drop_duplicates(subset = ["idequipos"])
+            self.sub_data[i] = self.sub_data[i].drop_duplicates(subset = ["idequipos"])
             
             self.enc_X_data[i]["tiempo"] = change_date(self.sub_data[i], self.data2["new_date"])
             
@@ -600,7 +351,7 @@ class my_main():
         for i in col_names:
             for j in range(len(self.sub_data[i])):
                 
-                new_table.append((predict_data[i][j][0],self.data2["new_date"],int(self.sub_data[i]["id"].to_numpy()[j])))
+                new_table.append((predict_data[i][j][0],self.data2["new_date"],int(self.sub_data[i]["idequipos"].to_numpy()[j])))
                 
         table_name = "predicciones"
         variable_name = ["predición","fecha","equipos_idequipos"]
@@ -608,22 +359,48 @@ class my_main():
 
         exists = self.Admin.create_table(table_name, variable_name, types)  
         
-        if exists: self.Admin.add_foreigh_key("equipos",table_name)
-
-        "insertando o actualizando los datos en la tabla"
-        
-        bar2 = Bar('update information: ', max = len(new_table))
-        for i in new_table:
+        #inserta todos los datos
+        if exists: 
+            self.Admin.add_foreigh_key("equipos",table_name)
             
-            # este vector es para tomar la decision entre insertar la informacion o actualizarla    
-            self.Admin.add_info(table_name,variable_name,i,exists)
-            bar2.next()
+            for i in new_table:
+                self.Admin.add_info(table_name,variable_name,i,exists)
         
-        bar2.finish()
-    
+        else:
+            
+            bar2 = Bar('update information: ', max = len(new_table))
+            
+            query = "SELECT * FROM {}".format(table_name)
+            
+            table = conect.run_query(query,self.db)
+            
+            id_table = table["equipos_idequipos"].to_numpy()
+                        
+            for i in new_table:
+                
+                if i[2] in id_table:
+                    exists = False
+                else:
+                    exists = True
+                
+                # este vector es para tomar la decision entre insertar la informacion o actualizarla    
+                self.Admin.add_info(table_name,variable_name,i,exists)
+                bar2.next()
+        
+            bar2.finish()
+
+        self.out = id_table,new_table
+        "insertando o actualizando los datos en la tabla"
+
     def get_matrix(self, result, num, training):
         # carga la informacion
-        
+        """[summary]
+
+        Args:
+            result ([lista de tuplas]): [resultado de la consulta]
+            num    ([int]): [numero minimo de consultas]
+            training ([bool]): [estado]
+        """
         if training: 
             # variables donde se encontrara el encoder
             self.enc_X = {}
@@ -635,14 +412,9 @@ class my_main():
         
         self.days_data = {}
         
-        obj_org = organizer(result,self.columns)
-        sort_data = obj_org.sort()
-
         #subdivimos la muestra
-        obj_sub = subdivider(sort_data, sort_data["categoria"])
+        obj_sub = subdivider(result, result["categoria"])
         self.sub_data = obj_sub.Subdivide(num)
-        
-        self.out = sort_data
         
         #columna a realizar encoder 
         columns_names = self.sub_data.keys()
@@ -667,7 +439,7 @@ class my_main():
             
             
             dic = {}
-            dic["id"] = self.days_data[i]["id"]
+            dic["idequipos"] = self.days_data[i]["idequipos"]
             
             for i_names in self.names:
                     
@@ -682,11 +454,11 @@ class my_main():
             rango = list(range(l))
             index = pd.Index(rango)
             
-            
             self.enc_X_data[i] = self.enc_X_data[i].set_index([index])            
         
     def predict_few(self):
-        
+        """[predice los id indicados en predict.json]
+        """
         word = "equi.idequipos = {}"
         self.my_query += " AND ("
         
@@ -698,7 +470,7 @@ class my_main():
         self.my_query = self.my_query[:len(self.my_query) - 2]
         self.my_query += " )"
         
-        result = self.Admin.query(self.my_query)
+        result = conect.run_query(self.my_query, self.db)
         
         self.get_matrix(result,num = 1, training = False)
         
@@ -710,5 +482,33 @@ class my_main():
         return self.out
 
 if __name__ == "__main__":
-    obj = my_main()
+    
+    ssh_host =     '107.180.51.23'
+    ssh_username = 'zda7zbr6kwzz'
+    ssh_password = 'Agosto1994.'
+    
+    # database information
+    
+    database_username = 'axel_garcia'
+    database_password = '4x3lg4rc14'
+    database_name =     'axel_test'
+    localhost =         '127.0.0.1'
+    
+    # link progress
+    
+    tunel = conect.open_ssh_tunnel(ssh_host,
+                                   ssh_username,
+                                   ssh_password,
+                                   verbose = False)
+    
+    connection = conect.mysql_connect(database_username,
+                                      database_password,
+                                      database_name,
+                                      tunel)
+    
+    cursor = connection.cursor()
+    
+    obj = my_main(cursor,connection)
+    
+    #obj = my_main(connection)
     
